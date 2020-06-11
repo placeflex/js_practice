e38ce18e9861fe97680ebcfcb18e7197f256e017
Egor Safronov <adelente1@gmail.com>
2020-06-11 15:07:28 +0300

    add js tabs code

---
+++ js-tabs/dist/js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsWrapper = document.querySelector('.tabheader__items');
+
+  function hideTabsContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show');
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabsContent(i = 0) {
+    tabsContent[i].classList.add('show');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  tabsWrapper.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (item == target) {
+          hideTabsContent();
+          showTabsContent(i);
+        }
+      });
+    }
+  });
+
+  hideTabsContent();
+  showTabsContent();
+});
